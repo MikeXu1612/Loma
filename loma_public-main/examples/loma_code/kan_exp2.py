e2e2b18935d3ea8e3edfd9d9801d8bcc99fbe2e4
Macrodove <2413923674@qq.com>
2025-06-10 17:44:29 -0700

    updated vector output test

--- loma_public-main/examples/loma_code/kan_exp2.py
+++ loma_public-main/examples/loma_code/kan_exp2.py
@@ -1,7 +1,2 @@
 def kan_network(X: In[Array[float, 2]], W: In[Array[float, 12]], A: In[Array[float, 30]], Y: Out[Array[float, 2]]):
-    a: float
-    b: float
-    a = kan(X, 2, 2, [3], 6, W, A)[0]
-    b = kan(X, 2, 2, [3], 6, W, A)[1]
-    Y[0] = a
-    Y[1] = b
+    kan(a, 2, 2, [3], 6, W, A, Y)
